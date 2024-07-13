907e53bcd86d29068abfdfc3d55118bf8cb46dd7
Ardit Daku <ardit.daku@musala.com>
2024-07-13 14:02:13 +0200

    Add home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 30.0),
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 120.0),
+            child: Text(
+              'What would you like to find?',
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
